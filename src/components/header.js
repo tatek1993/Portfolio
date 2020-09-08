5847b8d03728f0924bf4f2294a845cf1b3e2c277
Tate Keller <tatek1993@gmail.com>
2020-09-08 01:48:57 -0500

    set up skeleton for navigation, added styling to header, implemented typed.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import image from '../images/tate-keller-1.jpg';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import * as All from '../../node_modules/@fortawesome/fontawesome-free/css/all.css';
+
 import Typed from 'typed.js';
 
 const Example = (props) => {
@@ -13,26 +14,42 @@ const Example = (props) => {
     useEffect(() => {
 
         var typed = new Typed(headerIntro.current, {
-            strings: ["I'm a Minneapolis-based Python lover", "I'm a Minneapolis-based designer", "I'm a Minneapolis-based problem solver", "I'm a Minneapolis-based dad-joke connoisseur", "I'm a Minneapolis-based Web Developer"],
+            strings: [
+                "I'm a Minneapolis-based<br/>  <span class='red'>Python lover</span>,",
+                "I'm a Minneapolis-based<br/>  <span class='purple'>designer</span>,",
+                "I'm a Minneapolis-based<br/>  <span class='yellow'>problem solver</span>,",
+                "I'm a Minneapolis-based<br/>  <span class='orange'>dad-joke connoisseur</span>,",
+                "I'm a Minneapolis-based<br/>  <span class='green'>Web Developer</span>."],
             smartBackspace: true,
             typeSpeed: 30,
         });
     }, [])
 
-
-
-
-
     return (
 
         <div className="header">
-            <div>
-                <h1> Hi, I'm Tate!</h1>
-                <div className='header-text' >
-                    <span ref={headerIntro}>
-                    </span>
+            <div className="header-text-container">
+                <div>
+                    <h1 className="name"> Hi, I'm <span className="tate" >{'{'}</span>Tate<span className="tate" >{'}'}</span>.</h1>
+                    <div className='header-text' >
+                        <div className="header-typed">
+                            <span ref={headerIntro} />
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="dropdown">
+                    <div class="dropbtn"><i class="fas fa-angle-down" /> <i class="fas fa-folder icon" /> Portfolio</div>
+                    <div class="dropdown-content">
+                        <a href="#"><i class="fab fa-python blue" /> About Me</a>
+                        <a href="#"><i class="fab fa-js-square yellow" /> Skills</a>
+                        <a href="#"><i class="fab fa-css3 purple" /> Projects</a>
+                        <a href="#"><i class="fas fa-code-branch red" /> Contact</a>
+                    </div>
                 </div>
             </div>
+
             <header >
                 <img src={image} className="Main-image" alt="Tate Keller" />
             </header>
@@ -41,4 +58,3 @@ const Example = (props) => {
     );
 }
 export default Example;
-// <i class="fas fa-folder"></i>
