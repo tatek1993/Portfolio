6a1de1c688b2af2d55cf8293bd08d7737c98108d
Tate Keller <tatek1993@gmail.com>
2020-09-28 20:22:36 -0500

    updating projects.js

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -6,7 +6,7 @@ const About = (props) => {
 
         <div className='about border-box'>
             <div className="about-text">
-                <h2><i class="fas fa-angle-down chevron" /><span>.bio</span> {'{'} </h2>
+                <h2><i class="fas fa-angle-down chevron" /><span className="green">.bio</span> {'{'} </h2>
                 <p className="bio">
                     My eyes exploring the city sleeping late listening to music. My favorite word is going to shows really hoppy beers I'm a good listener bacon, I value art feminism I enjoy recently moved back I'm pretty laid-back. Thinking about trying yoga Vampire Weekend food trying different restaurants outdoorsy going back to school.
                 </p>
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,11 +2,44 @@ import React, { useState, useRef } from 'react';
 
 const Projects = () => {
 
+    const projects = [
+        {
+            image: ['yellow-bg'],
+            title: "Game Of Life",
+            description: "An interactive in-browser version of Conway's Game Of Life made using HTML Canvas.",
+            github: "https://github.com/tatek1993/Game-Of-Life"
+        },
+        {
+            image: ['yellow-bg'],
+            title: "Medical Cabinet Back End",
+            description: "A Node.js and Express restful API for the Medical Cabinet application, medical cannabis strain suggester.",
+            github: "https://github.com/tatek1993/medical-cabinet-back-end"
+        },
+        {
+            image: ['yellow-bg'],
+            title: "Canary Animation Test",
+            description: "A sample layout and animation mock-up for the marketing page of an investing/crowdfunding app.",
+            github: "https://github.com/tatek1993/Canary-Animation-WIP"
+        },
+    ]
 
 
     return (
-        <div className="projects border-box">
+        <div className="about border-box">
+            <h2><i class="fas fa-angle-down chevron" /><span className="red">.projects</span> {'{'} </h2>
+            <div className="projects-list">
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {
+                        projects.map(x => (<div className={`skill border-box  ${x.color}`}>
+                            {x.title}<br></br>
+                            {x.description}
+                        </div>))
+                    }
+                </div>
+            </div>
 
+
+            <h2>{'}'} </h2>
         </div>
     );
 };
