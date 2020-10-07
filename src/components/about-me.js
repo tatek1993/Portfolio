95af281db9836c71f0fae29c07cfc17cf32e7f82
Tate Keller <tatek1993@gmail.com>
2020-10-07 18:16:54 -0500

    updating about me section

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -8,7 +8,12 @@ const About = (props) => {
             <div className="about-text">
                 <h2><i class="fas fa-angle-down chevron" /><span className="green">.bio</span> {'{'} </h2>
                 <p className="bio">
-                    Hello there, my name is Tate Keller-Moctezuma. I'm a front end web developer in Minneapolis, MN specializing in React. I have a passion for writing clean, well-commented code and creating accessible user experiences that don't sacrifice style for function.
+                    Hello there, my name is Tate Keller-Moctezuma. 
+                    
+                    I'm a creative designer who's in it for the Aha! moments, spurred on when I can improve and simplify a solution at the same time. I enjoy breaking down the relationships between users and the web services they interact with.
+                    <br></br>
+                    When I'm not brainstorming side projects, you can find me gardening, painting, and playing tug-of-war with my dog.
+                    
                 </p>
                 <h2>{'}'} </h2>
             </div>
