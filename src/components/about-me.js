fa653ccffd5c2f011b785bc2a621469bc7e7a65a
Tate Keller <tatek1993@gmail.com>
2020-10-07 19:07:16 -0500

    updated bg color for projects

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -8,12 +8,14 @@ const About = (props) => {
             <div className="about-text">
                 <h2><i class="fas fa-angle-down chevron" /><span className="green">.bio</span> {'{'} </h2>
                 <p className="bio">
-                    Hello there, my name is Tate Keller-Moctezuma. 
-                    
-                    I'm a creative designer who's in it for the Aha! moments, spurred on when I can improve and simplify a solution at the same time. I enjoy breaking down the relationships between users and the web services they interact with.
-                    <br></br>
-                    When I'm not brainstorming side projects, you can find me gardening, painting, and playing tug-of-war with my dog.
-                    
+
+                    <h3>Hello there, my name is Tate Keller-Moctezuma. </h3>
+
+                    I'm a creative designer who's in it for the Aha! moments, spurred on when I can improve and simplify a solution at the same time. I enjoy breaking down the relationships between users and the web services they interact with to give my designs a more natural flow.
+
+                    <br />
+                    When I'm not coding or brainstorming side projects, you can find me gardening, painting, and playing tug-of-war with my dog.
+
                 </p>
                 <h2>{'}'} </h2>
             </div>
